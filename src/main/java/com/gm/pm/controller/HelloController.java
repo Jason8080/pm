84d82e544c221030be8b8468cb158ab422a4cf94
Jason.lee <gmlee.boss@gmail.com>
2020-06-06 18:42:38 +0800

    提供登陆,锁屏,404,500页面

--- src/main/java/com/gm/pm/controller/HelloController.java
+++ src/main/java/com/gm/pm/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.gm.pm.controller;
 
+import com.gm.pm.entity.Login;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Jason
@@ -12,5 +14,25 @@ public class HelloController {
     @GetMapping(value = "/")
     public String index() {
         return "index";
+    }
+    @GetMapping(value = "/404")
+    public String four() {
+        return "404";
+    }
+    @GetMapping(value = "/500")
+    public String five() {
+        return "500";
+    }
+    @GetMapping(value = "/lock")
+    public String lockscreen() {
+        return "lockscreen";
+    }
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+    @PostMapping(value = "/login")
+    public String login(Login login) {
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/gm/pm/entity/Login.java
@@ -0,0 +1,12 @@
+package com.gm.pm.entity;
+
+import lombok.Data;
+
+/**
+ * @author Jason
+ */
+@Data
+public class Login {
+    private String name;
+    private String pass;
+}

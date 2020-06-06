e697119e37ae6fb6c73cf0d80be343da57bba55b
Jason.lee <gmlee.boss@gmail.com>
2020-06-07 00:48:22 +0800

    打包部署调整.

--- src/main/java/com/gm/pm/base/controller/HelloController.java
+++ src/main/java/com/gm/pm/base/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.Cookie;
@@ -19,8 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloController {
 
     @GetMapping(value = "/")
-    public String index(Model model) {
-        return "index";
+    public ModelAndView index(Model model) {
+        return new ModelAndView("index");
     }
     @GetMapping(value = "/lock")
     public String lockscreen() {

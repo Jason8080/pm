8bc505f66b7a10c523f569d61fde102b613099a4
Jason.lee <gmlee.boss@gmail.com>
2020-06-07 15:37:55 +0800

    独立无尽搜索实体.

---
+++ src/main/java/com/gm/pm/entity/Condition.java
@@ -0,0 +1,36 @@
+package com.gm.pm.entity;
+
+import lombok.Data;
+import org.springframework.util.StringUtils;
+
+/**
+ * 无尽搜索模式
+ *
+ * @author Jason
+ */
+@Data
+public class Condition {
+
+    private Integer choose;
+    private String likes;
+
+    public void likeEndless() {
+        if (!StringUtils.isEmpty(this.likes)) {
+            char[] chars = this.likes.toCharArray();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < chars.length; i++) {
+                sb.append("%");
+                sb.append(chars[i]);
+            }
+            sb.append("%");
+            this.likes = sb.toString();
+        }
+    }
+
+    public Condition likeRecover() {
+        if (!StringUtils.isEmpty(this.likes)) {
+            this.likes = this.likes.replaceAll("%", "");
+        }
+        return this;
+    }
+}
--- src/main/java/com/gm/pm/entity/ProjectCondition.java
+++ src/main/java/com/gm/pm/entity/ProjectCondition.java
@@ -7,26 +7,6 @@ import org.springframework.util.StringUtils;
  * @author Jason
  */
 @Data
-public class ProjectCondition {
-    private Integer choose;
-    private String likes;
+public class ProjectCondition extends Condition{
 
-    public void likeEndless() {
-        if(!StringUtils.isEmpty(this.likes)){
-            char[] chars = this.likes.toCharArray();
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < chars.length; i++) {
-                sb.append("%");
-                sb.append(chars[i]);
-            }
-            sb.append("%");
-            this.likes = sb.toString();
-        }
-    }
-    public ProjectCondition likeRecover() {
-        if(!StringUtils.isEmpty(this.likes)){
-            this.likes = this.likes.replaceAll("%", "");
-        }
-        return this;
-    }
 }

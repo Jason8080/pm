9ab166e77b9b0bcaec9144cd231c8479a5640eb2
Jason <xiaoku13141@163.com>
2020-06-13 10:37:54 +0800

    ...

--- src/main/java/com/gm/pm/config/JwtSessionManager.java
+++ src/main/java/com/gm/pm/config/JwtSessionManager.java
@@ -3,10 +3,14 @@ package com.gm.pm.config;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.session.mgt.SessionContext;
 import org.apache.shiro.web.servlet.Cookie;
+import org.apache.shiro.web.servlet.ShiroHttpServletRequest;
 import org.apache.shiro.web.servlet.SimpleCookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.apache.shiro.web.util.WebUtils;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.Serializable;
@@ -26,18 +30,28 @@ public class JwtSessionManager extends DefaultWebSessionManager {
         cookie.setHttpOnly(true);
         return cookie;
     }
-
     @Override
-    protected void onStart(Session session, SessionContext context) {
-//        super.onStart(session, context);
-        Serializable currentId = session.getId();
-        Cookie template = this.getSessionIdCookie();
-        Cookie cookie = new SimpleCookie(template);
-        String idString = currentId.toString();
-        cookie.setValue(idString);
-        HttpServletRequest request = WebUtils.getHttpRequest(context);
-        HttpServletResponse response = WebUtils.getHttpResponse(context);
-        cookie.saveTo(request, response);
+    protected Serializable getSessionId(ServletRequest request, ServletResponse response) {
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)) {
+            // 是否将sid保存到cookie，浏览器模式下使用此参数。
+            if (WebUtils.isTrue(request, "token")) {
+                HttpServletRequest rq = (HttpServletRequest) request;
+                HttpServletResponse rs = (HttpServletResponse) response;
+                Cookie template = getSessionIdCookie();
+                Cookie cookie = new SimpleCookie(template);
+                cookie.setValue(token);
+                cookie.saveTo(rq, rs);
+            }
+            // 设置当前session状态
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_SOURCE,
+                    ShiroHttpServletRequest.URL_SESSION_ID_SOURCE); // session来源与url
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID, token);
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_IS_VALID, Boolean.TRUE);
+            return token;
+        } else {
+            return super.getSessionId(request, response);
+        }
     }
 
 }
